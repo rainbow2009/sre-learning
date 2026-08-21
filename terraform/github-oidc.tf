0e6210b9b631b7a0f9de98d5d1b9b963569f2828
rainbow2009 <danilaodessa@gmail.com>
2026-08-21 04:55:49 +0300

    Add Helm chart and values

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
   tags            = local.common_tags
 }
 
@@ -21,7 +21,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:rainbow2009/sre-learning:*"
+          "token.actions.githubusercontent.com:sub" = "repo:rainbow2009*sre-learning*"
         }
       }
     }]
